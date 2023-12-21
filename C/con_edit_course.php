83cb3a2be455aadc44010343d80c554bfb03212a
worapakorn <120018217+WorapakornJarusiriphot@users.noreply.github.com>
2023-12-21 23:51:28 +0700

    UP
    UP

--- C/con_edit_course.php
+++ C/con_edit_course.php
@@ -1,18 +1,15 @@
-
 <?php
   include_once "../M/ConDB.php";
   include_once "../M/Tourist.php";
 
-  //สร้างตัวแปร
   $activity_id = $_POST['activity_id'];
   $activity_name = $_POST['activity_name'];
   $description = $_POST['description'];
   $date = $_POST['date'];
 
-  //เรียกใช้
-  $obj_name=new Tourist();
-  $rs2=$obj_name->editTouristeditTourist($activity_id, 
-    $activity_name,
-    $description,
-    $date,)
+  $obj_name = new Tourist();
+  $rs2 = $obj_name->editTouristeditTourist($activity_id, $activity_name, $description, $date);
+
+  header('Location: ../index.php?updated=true');
+  exit;
 ?>
